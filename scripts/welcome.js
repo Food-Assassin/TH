588045849234d31b908fb61d6e9b823867c49587
Food-Assassin <1324049024@qq.com>
2022-09-05 09:58:32 +0800

    z

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -1,11 +1,38 @@
 import * as mc from "mojang-minecraft"
+import * as ui from 'mojang-minecraft-ui'
 
-mc.world.events.beforeChat.subscribe(z => {
-    if (z.message = 'test') {
-        
-    }
-})
+var i = 0
+var player
+var pn = Array.from(mc.world.getPlayers()).length
+function welcome() {
+    var welcome = new ui.MessageFormData
+    welcome = welcome.title('洛卷卷')
+    welcome = welcome.body(`${player.name}，欢迎回来！`)
+    welcome = welcome.button1('关闭')
+    welcome = welcome.button2('关闭')
+    welcome = welcome.show(player).then((arg) => {
+        if (arg.selection == 0) {
+            pn = Array.from(mc.world.getPlayers()).length
+        } else if (arg.selection == 1) {
+            pn = Array.from(mc.world.getPlayers()).length
+        }
+    })
+}
 
 mc.world.events.playerJoin.subscribe(p => {
-    p.player.runCommandAsync('say 欢迎回来！')
+    player = p.player
 })
+
+mc.world.events.tick.subscribe(t => {
+    if (i >= 40) {
+        if (pn < Array.from(mc.world.getPlayers()).length) {
+                welcome()
+            } else {
+                pn = Array.from(mc.world.getPlayers()).length
+            }
+            i = 0
+    } else {
+        i++
+    }
+    }
+)
